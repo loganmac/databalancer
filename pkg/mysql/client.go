313dde3462ece8501eb3db31cd6f61e5298976e7
Logan McPhail <logan.airnomad@gmail.com>
2018-05-18 19:07:09 -0600

    support dynamic table creation and inserts

--- pkg/mysql/client.go
+++ pkg/mysql/client.go
@@ -5,25 +5,25 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/kolide/databalancer-logan/pkg/db"
+	_ "github.com/go-sql-driver/mysql" //mysql driver
 	"github.com/kolide/databalancer-logan/pkg/logs"
 	"github.com/pkg/errors"
 )
-
-// Table defines methods for inserting and querying logs for that table
-type Table struct {
-	insert *sql.Stmt   // prepared statement for inserting
-	family logs.Family // family (table name) for the table
-	schema logs.Schema // schema of the table from request
-}
 
 // Client is a connection to a MySQL database
 type Client struct {
 	*sql.DB // underlying database
 }
 
-// Create makes a new MySQL database client and ensures that it's connected
-func Create(username, password, address, name string) (*Client, error) {
+// Table defines methods for inserting and querying logs for that table
+type Table struct {
+	*sql.DB                   // database for table
+	name    string            // table name
+	schema  map[string]string // schema of the table from request
+}
+
+// NewClient makes a new MySQL database client and ensures that it's connected
+func NewClient(username, password, address, name string) (*Client, error) {
 	connectionString := fmt.Sprintf(
 		"%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		username,
@@ -43,50 +43,37 @@ func Create(username, password, address, name string) (*Client, error) {
 	}
 
 	log.Printf("Connected to MySQL as %s at %s\n", username, address)
-
 	return &Client{DB: db}, nil
 }
 
-// FindOrCreateTable finds or creates the table based on the given
-// attributes with the client, caching it in the process
-func (c *Client) FindOrCreateTable(family logs.Family, schema logs.Schema) (db.Table, error) {
-	create, err := c.Prepare(
-		"CREATE TABLE IF NOT EXISTS `raw_logs` (" +
-			"`id` INT NOT NULL AUTO_INCREMENT," +
-			"`family` TEXT," +
-			"`log` TEXT," +
-			"PRIMARY KEY(`id`)" +
-			");",
-	)
+// CreateTable creates the the table (if it doesn't exist) based on the given
+// attributes with the client and creates an Insert method.
+func (c *Client) CreateTable(name logs.Family, schema logs.Schema) (logs.Table, error) {
+	// construct create table statement
+	create := CreateTableStatement(name.String(), schema)
+
+	// create the table with the template args
+	log.Printf("Create statment: \n%+v\n", create)
+	_, err := c.Exec(create)
 	if err != nil {
-		return nil, errors.Wrap(err, "preparing create statement")
+		return nil, errors.Wrapf(err, "creating %s table", name)
 	}
 
-	// create the table
-	_, err = create.Exec()
-	if err != nil {
-		return nil, errors.Wrap(err, "executing create statement")
-	}
-
-	// create the insert prepared statement
-	insert, err := c.Prepare("INSERT INTO `raw_logs` (`family`, `log`) VALUES(?, ?);")
-	if err != nil {
-		return nil, errors.Wrap(err, "creating insert statement")
-	}
-
-	return &Table{insert: insert, family: family, schema: schema}, nil
+	return &Table{DB: c.DB, name: name.String(), schema: schema}, nil
 }
 
-// Insert creates a new record in the supplied table
-func (t *Table) Insert(log []byte) error {
-	res, err := t.insert.Exec(t.family.String(), log)
-	if err != nil {
-		return errors.Wrap(err, "executing insert statement")
-	}
+// Insert creates new logs in the supplied table
+func (t *Table) Insert(logs logs.Raw) error {
+	// construct insert statement
+	insert, args := InsertTableStatement(t.name, t.schema, logs)
 
-	// verify record was inserted
-	if _, err := res.LastInsertId(); err != nil {
-		return errors.Wrap(err, "inserting record")
+	log.Printf("Insert statment: \n%+v\n", insert)
+	log.Printf("Args: \n%+v\n", args)
+
+	// insert the data
+	_, err := t.Exec(insert, args...)
+	if err != nil {
+		return errors.Wrapf(err, "inserting records for %s table", t.name)
 	}
 
 	return nil
---
+++ pkg/mysql/statements.go
@@ -0,0 +1,128 @@
+package mysql
+
+import (
+	"strings"
+)
+
+// CreateTableStatement builds a create table statement string from a
+// table name and a schema. Note that the table will have an INT typed `id`
+// primary key
+func CreateTableStatement(name string, schema map[string]string) string {
+	// list of fields in the schema
+	var tableFields []string
+	for fieldName, fieldType := range schema {
+		// append field field name and appropriate field type to field list
+		switch fieldType {
+		case "string":
+			field := "`" + Escape(fieldName) + "` TEXT, "
+			tableFields = append(tableFields, field)
+		case "int":
+			field := "`" + Escape(fieldName) + "` INT, "
+			tableFields = append(tableFields, field)
+		}
+	}
+	safeTableFields := strings.Join(tableFields, "")
+
+	stmt := "CREATE TABLE IF NOT EXISTS `" +
+		Escape(name) +
+		"`(`id` INT NOT NULL AUTO_INCREMENT, " +
+		safeTableFields +
+		"PRIMARY KEY(`id`)" +
+		");"
+
+	return stmt
+}
+
+// InsertTableStatement builds a statement to insert records into a table,
+// given a table name, schema, and some records, and returns the arguments to be passed
+// to the statement
+// NOTE: I figured it was safer and better to use the built-in mechanism (bindvars) for
+// record value inserts, and only handle escaping the table name and field names manually.
+func InsertTableStatement(name string, schema map[string]string, records []map[string]interface{}) (string, []interface{}) {
+	// list of field names (to preserve order bewteen field names and arguments)
+	var fieldNames []string
+	// will represent placeholders for fields
+	var bindvars []string
+	for fieldName := range schema {
+		// append a bindvar for the field
+		bindvars = append(bindvars, "?")
+		// append safe field name and to list of fields
+		fieldNames = append(fieldNames, Escape(fieldName))
+	}
+	// concatenate the field names and wrap in backticks
+	var safeTableFields = "`" + strings.Join(fieldNames, "`, `") + "`"
+
+	// concatenate the bindvars and wrap in parens
+	var bindvarString = "(" + strings.Join(bindvars, ", ") + ")"
+
+	// the list of bindvars for all records
+	var valueBindvars []string
+	// the list of args to pass into the statement
+	var args []interface{}
+	for _, record := range records {
+		// append a bindvarstring for each record
+		valueBindvars = append(valueBindvars, bindvarString)
+
+		for _, fieldName := range fieldNames {
+			// append field values as arguments
+			args = append(args, record[fieldName])
+		}
+	}
+	// join the value bind vars
+	valuePlaceholders := strings.Join(valueBindvars, ", ")
+
+	stmt := "INSERT INTO `" +
+		Escape(name) +
+		"`(" +
+		safeTableFields +
+		") VALUES " +
+		valuePlaceholders +
+		";"
+
+	return stmt, args
+}
+
+// Escape prepares strings to be safely used in MySQL statements
+// I found this from a quick google search. For the sake of time,
+// I'm just going to trust this. Ideally, it would have lots of tests
+//  to protect from injection attacks.
+func Escape(sql string) string {
+	dest := make([]byte, 0, 2*len(sql))
+	var escape byte
+	for i := 0; i < len(sql); i++ {
+		c := sql[i]
+
+		escape = 0
+
+		switch c {
+		case 0: /* Must be escaped for 'mysql' */
+			escape = '0'
+			break
+		case '\n': /* Must be escaped for logs */
+			escape = 'n'
+			break
+		case '\r':
+			escape = 'r'
+			break
+		case '\\':
+			escape = '\\'
+			break
+		case '\'':
+			escape = '\''
+			break
+		case '"': /* Better safe than sorry */
+			escape = '"'
+			break
+		case '\032': /* This gives problems on Win32 */
+			escape = 'Z'
+		}
+
+		if escape != 0 {
+			dest = append(dest, '\\', escape)
+		} else {
+			dest = append(dest, c)
+		}
+	}
+
+	return string(dest)
+}
